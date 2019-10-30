1d4e680b77dd610076af675e502efdd0a8823500
cesar rojas <cesar.fieoner@gmail.com>
2019-10-30 22:02:28 +0100

    haha

--- BGAnimations/ScreenEvaluation common/PerPlayer/ScatterPlot.lua
+++ BGAnimations/ScreenEvaluation common/PerPlayer/ScatterPlot.lua
@@ -32,7 +32,6 @@ local worst_window = SL.Preferences[SL.Global.GameMode]["TimingWindowSecondsW"..
 for t in ivalues(sequential_offsets) do
 	CurrentSecond = t[1]
 	Offset = t[2]
-	SM(t)
 
 	if not string.find(Offset ,"Miss.") then
 		CurrentSecond = CurrentSecond - Offset
